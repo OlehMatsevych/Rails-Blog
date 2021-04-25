f208bcae0094d5740bb351c596baa4767f0640d7
oleg-_-macevych <oleg_macevich@icloud.com>
2021-04-26 01:37:06 +0300

    registration.. added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 2)
     end
     def show
         @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     def update
         @user = User.find(params[:id])
